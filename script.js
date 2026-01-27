4414d6ce3902d0d565637e2cb2c82ebb07f5bdf4
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-27 23:00:37 +0100

    Add click event to button for message display

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ button {
   font-size: 18px;
   cursor: pointer;
 }
+const bouton = document.getElementById("monBouton");
+const message = document.getElementById("message");
+
+bouton.addEventListener("click", function () {
+  message.textContent = "Bravo 🎉 Tu viens de cliquer sur le bouton !";
+});
