fde3e65a3ca3a2e7207f903722c283122e26a2b8
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-27 23:24:42 +0100

    Change button click message to English

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const bouton = document.getElementById("monBouton");
 const message = document.getElementById("message");
 
 bouton.addEventListener("click", function () {
-  message.textContent = "Bravo 🎉 Tu viens de cliquer sur le bouton !";
+  message.textContent = "congrats ! You just pressed a button";
 });
