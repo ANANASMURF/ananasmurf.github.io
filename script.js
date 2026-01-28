b68004aa67191e4344b06764dac4c4f6fac955cb
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-28 17:42:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ bouton2.addEventListener("click", function () {
   window.location.href = "pageno.html";
 });
 bouton3.addEventListener("click", function () {
-  window.location.href = "indexb.html";
+  window.location.href = "scriptb.js";
 });
 
