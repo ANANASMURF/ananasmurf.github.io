af9158ed0da709610a39a77dcd40f846fd7533e7
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-28 22:17:08 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,26 @@ const bouton2 = document.getElementById("monBouton2");
 const bouton3 = document.getElementById("monBoutontest");
 const bouton4 = document.getElementById("monBoutonBacktomenu");
 
-bouton1.addEventListener("click", function () {
-  window.location.href = "page.html";
-});
-bouton2.addEventListener("click", function () {
-  window.location.href = "pageno.html";
-});
-bouton3.addEventListener("click", function () {
-  window.location.href = "icedodo.html";
-});
-bouton4.addEventListener("click", function () {
-  window.location.href = "index.html";
-});
+if (bouton1) {
+  bouton1.addEventListener("click", function () {
+    window.location.href = "page.html";
+  });
+}
 
+if (bouton2) {
+  bouton2.addEventListener("click", function () {
+    window.location.href = "pageno.html";
+  });
+}
+
+if (bouton3) {
+  bouton3.addEventListener("click", function () {
+    window.location.href = "icedodo.html";
+  });
+}
+
+if (bouton4) {
+  bouton4.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+}
