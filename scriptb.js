9aaad134853e319e6845f8c413e4072d8e56535e
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-28 17:30:43 +0100

    Create scriptb.js

---
+++ scriptb.js
@@ -0,0 +1,33 @@
+const player = document.getElementById("player");
+const target = document.getElementById("target");
+
+let x = 10;
+let y = 10;
+const step = 10;
+
+document.addEventListener("keydown", function (event) {
+
+  if (event.key === "ArrowUp") {
+    y -= step;
+  }
+  if (event.key === "ArrowDown") {
+    y += step;
+  }
+  if (event.key === "ArrowLeft") {
+    x -= step;
+  }
+  if (event.key === "ArrowRight") {
+    x += step;
+  }
+
+  player.style.left = x + "px";
+  player.style.top = y + "px";
+
+  checkCollision();
+});
+
+function checkCollision() {
+  if (x === 300 && y === 200) {
+    document.getElementById("message").textContent = "🎉 Gagné !";
+  }
+}
