387fede5bc50288a3e49172dcd38f2e635e68f78
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-28 10:07:13 +0100

    Add second button element to script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const bouton = document.getElementById("monBouton");
 const message = document.getElementById("message");
+const bouton = document.getElementById("monBouton2");
 
 bouton.addEventListener("click", function () {
   window.location.href = "page.html";
