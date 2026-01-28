caf31701a55357d4f9ed15ffa9b87a06c74bc6e8
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-28 10:19:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const bouton = document.getElementById("monBouton");
+const bouton1 = document.getElementById("monBouton");
 const message = document.getElementById("message");
-const bouton = document.getElementById("monBouton2");
+const bouton2 = document.getElementById("monBouton2");
 
-bouton.addEventListener("click", function () {
+bouton1.addEventListener("click", function () {
   window.location.href = "page.html";
 });
+bouton2.addEventListener("click", function () {
+  window.location.href = "pageNO.html";
+});
+
