438858a1e63332929906096b15a6e483a105a2cc
ANANASMURF <mauvaiseelevedesaintanne@gmail.com>
2026-01-28 17:35:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const bouton1 = document.getElementById("monBouton");
 const bouton2 = document.getElementById("monBouton2");
+const bouton3 = document.getElementById("monBoutontest");
 
 bouton1.addEventListener("click", function () {
   window.location.href = "page.html";
 });
 bouton2.addEventListener("click", function () {
   window.location.href = "pageno.html";
+});
+bouton3.addEventListener("click", function () {
+  window.location.href = "indexb.html";
 });
 
